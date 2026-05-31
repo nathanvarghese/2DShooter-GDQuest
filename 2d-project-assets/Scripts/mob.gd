49d585df3df59792dcf06fe74ba4df1d8ac21096
Nathan Varghese <nathanvarghese948@gmail.com>
2026-05-31 12:14:08 -0600

    base game + scripts folder

---
+++ 2d-project-assets/Scripts/mob.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+var health = 3
+
+@onready var player = get_node("/root/Game/Player")
+
+func _ready():
+	%Slime.play_walk()
+
+func _physics_process(_delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 300
+	move_and_slide()
+
+func take_damage():
+	%Slime.play_hurt()
+	health -= 1
+	
+	if health == 0:
+		var smoke_scene = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke = smoke_scene.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
+		queue_free()
