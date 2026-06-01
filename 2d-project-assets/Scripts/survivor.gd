6346d836f4fd78fda49a0dedd35e0ad28f0b0ffb
Nathan Varghese <nathanvarghese948@gmail.com>
2026-05-31 21:11:08 -0600

    small changes

--- 2d-project-assets/Scripts/survivor.gd
+++ 2d-project-assets/Scripts/survivor.gd
@@ -11,7 +11,6 @@ func spawn_mob():
 	%PathFollow2D.progress_ratio = randf()
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
-	
 
 func _on_timer_timeout() -> void:
 	spawn_mob()
